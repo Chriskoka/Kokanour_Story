7e8becc0fccdc3095bd7a8537fdfa24a88bf8342
Chris Kokanour <christopherkok.31460@redlandsschools.net>
2018-09-10 15:26:05 +0000

    First Python

---
+++ second.py
@@ -0,0 +1,6 @@
+age = input('Please type your age here ----> ')
+
+if (age > 17) :
+    print ('Please vote who you think is the best out of the following list: Chris, Chris, Chris, and Chris')
+if (age < 18) :
+    print('Please go away, you are not of age to vote')
